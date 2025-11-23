789b630f30f164824426ac53b706f8f11882dd8d
OsafAliSayed <aousafalisayedb2@gmail.com>
2025-11-23 21:28:45 +0530

    feat: add posthog public key

--- app/provider.jsx
+++ app/provider.jsx
@@ -10,14 +10,16 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 import { ThemeProvider } from "next-themes";
 
+import { POSTHOG_KEY, POSTHOG_HOST } from "@/lib/posthog"
+
 export function Providers({ children }) {
   return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>;
 }  
- 
+
 export function PostHogProvider({ children }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    posthog.init(POSTHOG_KEY, {
+      api_host: POSTHOG_HOST,
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-05-24'
     })
